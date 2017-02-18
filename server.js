5265c3a0bb1df4125b4499bd1931afba9d4d427a
nefaerious <samshin102@gmail.com>
2017-02-18 15:49:09 -0600

    python tests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var routes = require('./routes/routes');
+var python = require('python-shell');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -12,4 +13,10 @@ app.use(function(req, res) {
 
 var server = app.listen(app.get('port'), function() {
   console.log('Node server started on port ' + app.get('port'));
+});
+
+console.log("Hi. We're starting our python-shell tests.");
+python.run("main.py", function(err, results) {
+  if (err) throw err;
+  console.log(results);
 });
