81bf18bbbad1e19ff0e178c14d281cede1785033
Thai Nguyen <trevortknguyen@gmail.com>
2017-02-19 08:10:56 -0600

    Add logging

--- test/graphdb.js
+++ test/graphdb.js
@@ -32,6 +32,8 @@ describe('graphene database', () => {
   describe('getAllNodes', () => {
     it('should get all nodes', (done) => {
       graphdb.getAllNodes((nodes) => {
+        console.log(nodes);
+        console.log(nodes[0]);
         done();
       });
     });
