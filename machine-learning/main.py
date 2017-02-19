fe6245ab6cd690316c0a7010793044f486867bad
nefaerious <samshin102@gmail.com>
2017-02-18 21:50:44 -0600

    removed py code for testing

--- machine-learning/main.py
+++ machine-learning/main.py
@@ -1,17 +1,18 @@
-import neurolab as nl
-import numpy as np
-
-# Create train samples
-x = np.linspace(-7, 7, 20)
-y = np.sin(x) * 0.5
-
-size = len(x)
-
-inp = x.reshape(size,1)
-tar = y.reshape(size,1)
-
-net = nl.net.newff([[-1.0, 1.0],[-1.0, 1.0],[-1.0, 1.0]], [3, 1])
-
-error = net.train(inp, tar, epochs=500, show=100, goal=0.02)
-
-out = net.sim(inp)
+# import neurolab as nl
+# import numpy as np
+#
+# # Create train samples
+# x = np.linspace(-7, 7, 20)
+# y = np.sin(x) * 0.5
+#
+# size = len(x)
+#
+# inp = x.reshape(size,1)
+# tar = y.reshape(size,1)
+#
+# net = nl.net.newff([[-1.0, 1.0],[-1.0, 1.0],[-1.0, 1.0]], [3, 1])
+#
+# net.train_gd(inp, tar, epochs-500, show=100, goal=0.02);
+#
+# out = net.sim(inp)
+print("hello world")
