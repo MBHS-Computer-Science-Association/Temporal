d19c38f46a3195a71ec83af2d1cc2dc88e07f57e
nefaerious <samshin102@gmail.com>
2017-02-18 19:36:41 -0600

    changes

--- machine-learning/main.py
+++ machine-learning/main.py
@@ -1,3 +1,17 @@
 import neurolab as nl
+import numpy as np
 
-print("hello world")
+# Create train samples
+x = np.linspace(-7, 7, 20)
+y = np.sin(x) * 0.5
+
+size = len(x)
+
+inp = x.reshape(size,1)
+tar = y.reshape(size,1)
+
+net = nl.net.newff([[-1.0, 1.0],[-1.0, 1.0],[-1.0, 1.0]], [3, 1])
+
+error = net.train(inp, tar, epochs=500, show=100, goal=0.02)
+
+out = net.sim(inp)
