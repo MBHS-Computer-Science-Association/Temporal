dbd799e5f471ca32b97938a340e7fd503ee3500f
nefaerious <samshin102@gmail.com>
2017-02-19 00:49:11 -0600

    socket io changes

--- public/quiz.js
+++ public/quiz.js
@@ -1,5 +1,17 @@
 var socket = io();
 
+socket.emit('imhere', 5);
+
+socket.on('responsefromscaredserver', (theirresponse) => {
+
+});
+
+$('form').submit(function(){
+    socket.emit('chat message', $('#').val());
+    $('#m').val('');
+    return false;
+});
+
 $('.button').click(function(){
   document.getElementById('label4').innerHTML = 'hello';
 });
