6debfd52a3a798cf0de922d8e5a523f756c429d0
Will Robinson <sumodude529@gmail.com>
2017-02-19 00:26:51 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
 router.use(express.static('public'));
-
-router.set('views', path.join(__dirname, 'public'));
-router.set('view engine', 'ejs');
 
 module.exports = router;
