75400d5b4aa25cec168b69bfb9d0e069ea66c9a8
Thai Nguyen <trevortknguyen@gmail.com>
2017-02-18 21:56:44 -0600

    Add direct test log

--- test/graphdb.js
+++ test/graphdb.js
@@ -2,6 +2,12 @@ var assert = require('chai').assert;
 
 var graphdb = require('../graphdb');
 
+
+graphdb.createNode("Ukrainian Uprising", "Ukrainians in Crimea were misbehaving.", (id) => {
+  console.log("The new node id: " + id);
+  assert(typeof id === 'number');
+});
+
 describe('graphene database', () => {
   describe('createNode', () => {
     it('should create a node', () => {
