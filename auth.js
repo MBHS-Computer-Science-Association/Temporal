62f828acfe3efcb156527e7a839ba419c3044b2a
Jeffrey Wang <j@mywikis.com>
2017-02-19 02:26:36 -0600

    Postgres COUNT

--- auth.js
+++ auth.js
@@ -18,7 +18,7 @@ auth.login = (username, password) => {
   // search for user/pass combo
   // if found, return true
   // if not found, return false
-  var userFound = db.query('SELECT * FROM users WHERE username->>($1) AND password->>($2);', username, passwordhash(password) );
+  var userFound = db.query('SELECT COUNT(*) FROM users WHERE username->>($1) AND password->>($2);', username, passwordhash(password) );
   if ( userFound !== 1 ) {
     // user already exists
     return false; // error
@@ -28,7 +28,7 @@ auth.login = (username, password) => {
 
 auth.signup = (username, password, email) => {
   // ensure username is unique
-  var uniqueCheck = db.query('SELECT * FROM users WHERE username->>($1), row_number() OVER as rnum FROM users;', username );
+  var uniqueCheck = db.query('SELECT COUNT(*) FROM users WHERE username->>($1), row_number() OVER as rnum FROM users;', username );
   //var uniqueCheck = JSON.parse(uniqueCheckQuery);
   if ( uniqueCheck !== 0 ) {
     // user already exists
