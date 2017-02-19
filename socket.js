67bc760733e9f35233b4a2be9187cfe7a8f92842
nefaerious <samshin102@gmail.com>
2017-02-19 09:53:59 -0600

    fixes

--- socket.js
+++ socket.js
@@ -29,13 +29,14 @@ var exports = module.exports = function(server) {
           list = param;
         });
         var related;
-        // for(var i = 0; i < totalNodes; i++){
-        //   if(list[i].id == totalNodes){
-        //     rdb.getRelatedNodes(i, (info)=>{
-        //       related = info;
-        //     });//RANDOMIZE WHICH RELATE
-        //     data = [[list[i].name],[related[0]],[]];
-        // }
+        for(var i = 0; i < totalNodes; i++){
+          if(list[i].id == totalNodes){
+            rdb.getRelatedNodes(i, (info)=>{
+              related = info;
+            });//RANDOMIZE WHICH RELATE
+            data = [[list[i].name],[related[0]],[]];
+        }
+      }
         data = [["Russian Revolution"],["Vietnam War"],["both are wars"], ["Thai", "Trevor", "both are the same person"],["Renaissance","Scientific Revolution","both changed the world"],["Donald Trump", "Vladimir Putin", "both are working for Russia"]];
         console.log(data);
       socket.emit('data', data);
@@ -72,8 +73,6 @@ var exports = module.exports = function(server) {
     socket.on('request_final_graph', function(callback) {
       db.getSigmaGraph(callback);
     });
-
-  });
 
   var obj = {};
   return obj;
