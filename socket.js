1b6eae06e2d03dbee9c27a9503a62bfd442346e2
nefaerious <samshin102@gmail.com>
2017-02-19 07:28:08 -0600

    socket io server side for quiz

--- socket.js
+++ socket.js
@@ -1,13 +1,43 @@
+var db = require('./graphdb');
+
 var exports = module.exports = function(server) {
   var io = require('socket.io')(server);
+  var isQuizzing = false;
+
   io.on('connection', (socket) => {
     console.log("User connected.");
 
     socket.on('disconnect', () => {
       console.log("User disconnected.");
     });
-    socket.emit('data', [["Russian Revolution","Vietnam War","both are wars"], ["Thai", "Trevor", "both are the same person"], ["Renaissance","Scientific Revolution","both changed the world"], ["Donald Trump", "Vladimir Putin", "both are working for Russia"]]);
 
+    if(socket.on('quiz', (quiz) => {return isQuizzing = true;} == true){
+      var totalNodes = ;//use JEffrey database to see how many nodes in user set
+      var nextQuestion = false;
+      var isCorrect = false;
+      var data;
+      while(isQuizzing){
+
+      //TODO: do some array handling with the database
+      db.getAnswer()....
+      socket.emit('data', data);
+
+        while(!nextQuestion){
+        isCorrect = socket.on('answer', (answer) => {
+          if(answer == /* SOME CORRECT ANSWER */){
+            return true;
+          }
+        });
+        if(isCorrect){
+            socket.emit('nextQ', true);
+            nextQuestion = true;
+        }else{
+           socket.emit('nextQ', false);
+        }
+      }
+
+    }
+  }
 
     socket.on('answer', (ans) => {
       console.log(ans);
