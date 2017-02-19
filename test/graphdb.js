38c1df681a49ee24f1f82f42bc717f1af572bb1b
Thai Nguyen <trevortknguyen@gmail.com>
2017-02-19 02:57:13 -0600

    Modify tests for deleting nodes

--- test/graphdb.js
+++ test/graphdb.js
@@ -3,7 +3,7 @@ var assert = require('chai').assert;
 var graphdb = require('../graphdb');
 
 describe('graphene database', () => {
-  describe.only('createNode', () => {
+  describe('createNode', () => {
     it('should create a node', (done) => {
       graphdb.createNode("Ukrainian Uprising", "Ukrainians in Crimea were misbehaving.", (id) => {
         console.log("The new node id: " + id);
@@ -15,7 +15,7 @@ describe('graphene database', () => {
 
   describe('deleteNode', () => {
     it('should delete the node', (done) => {
-      graphdb.deleteNode(2, () => {
+      graphdb.deleteNode(0, () => {
         done();
       });
     });
@@ -23,7 +23,7 @@ describe('graphene database', () => {
 
   describe('getNode', () => {
     it('should get the node', (done) => {
-      graphdb.getNode(0, (node) => {
+      graphdb.getNode(1, (node) => {
         done();
       });
     });
@@ -39,7 +39,7 @@ describe('graphene database', () => {
 
   describe('nodeEditTitle', () => {
     it('should change the title', (done) => {
-      graphdb.nodeEditTitle(0, "China", () => {
+      graphdb.nodeEditTitle(1, "China", () => {
         // assert that the title is changed
         done();
       });
@@ -48,16 +48,16 @@ describe('graphene database', () => {
 
   describe('nodeEditDescription', () => {
     it('should change the description', (done) => {
-      graphdb.nodeEditDescription(0, "This is a dangerous country.", () => {
+      graphdb.nodeEditDescription(1, "This is a dangerous country.", () => {
         // assert that the description is changed
         done();
       });
     });
   });
 
-  describe('createRelationship', () => {
+  describe.only('createRelationship', () => {
     it('should create a new relationship with title and description', (done) => {
-      graphdb.createRelationship(0, 1, "Caused", "out of spite", (id) => {
+      graphdb.createRelationship(1, 2, "Caused", "out of spite", (id) => {
         console.log("The new relationship id: " + id);
         assert(typeof id === 'number');
         done();
@@ -67,7 +67,7 @@ describe('graphene database', () => {
 
   describe('relationshipEditTitle', () => {
     it('should change the title', (done) => {
-      graphdb.relationshipEditTitle(0, "Aggravated", () => {
+      graphdb.relationshipEditTitle(1, "Aggravated", () => {
         // assert that the title is changed
         done();
       });
@@ -76,7 +76,7 @@ describe('graphene database', () => {
 
   describe('relationshipEditDescription', () => {
     it('should change the description', (done) => {
-      graphdb.relationshipEditDescription(0, "This was a result of the lack of provisions.", () => {
+      graphdb.relationshipEditDescription(1, "This was a result of the lack of provisions.", () => {
         // assert that the description is changed
         done();
       });
