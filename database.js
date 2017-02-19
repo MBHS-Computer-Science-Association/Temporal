5a524ccd15c9b8f3c5d3532583cf5d6cbea1053b
Thai Nguyen <trevortknguyen@gmail.com>
2017-02-18 20:50:58 -0600

    Add logging

--- database.js
+++ database.js
@@ -9,6 +9,7 @@ var db = new pg.Client(process.env.DATABASE_URL | 'postgres://postgres@localhost
 
 db.connect(function(err) {
   if ( err ) {
+    console.log(process.env.DATABASE_URL | 'postgres://postgres@localhost/travis_ci_test');
     console.log(process.env.DATABASE_URL);
     console.log('postgres://postgres@localhost/travis_ci_test');
     throw err;
