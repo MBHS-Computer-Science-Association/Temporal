d906a924d5574fb578dfe65fabe4ece2787dd75c
Will Robinson <sumodude529@gmail.com>
2017-02-19 00:27:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 var routes = require('./routes/routes');
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
 
 app.use('/', routes);
 app.use(function(req, res) {
