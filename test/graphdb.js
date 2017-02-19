cd4399a99dce64c0c9c35f08d2cffa32ee65895a
Thai Nguyen <trevortknguyen@gmail.com>
2017-02-18 21:24:30 -0600

    Add graph database testing

---
+++ test/graphdb.js
@@ -0,0 +1,14 @@
+var assert = require('chai').assert;
+
+var graphdb = require('../graphdb');
+
+describe('graphene database', () => {
+  describe('createNode', () => {
+    it('should create a node', () => {
+      graphdb.createNode("Ukrainian Uprising", "Ukrainians in Crimea were misbehaving.", (id) => {
+        console.log("The new node id: " + id);
+        assert(typeof id === 'number');
+      });
+    });
+  });
+});
--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 var assert = require('chai').assert;
 
 var server = require('../server.js');
-var graphdb = require('../graphdb');
 
 describe('Temporal Server', function() {
   describe('server', function() {
