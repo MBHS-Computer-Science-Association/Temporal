8f81dc7bffd7723dabb355d7d626e651fff8033a
Thai Nguyen <trevortknguyen@gmail.com>
2017-02-19 10:50:19 -0600

    Add creation methods through socket.io

--- graphsocket.js
+++ graphsocket.js
@@ -10,4 +10,12 @@ var exports = module.exports = function(socket) {
   socket.on('request_final_graph', function(callback) {
     db.getSigmaGraph(callback);
   });
+
+  socket.on('create_node', (obj) => {
+    db.createNode(obj.title, obj.description);
+  });
+
+  socket.on('create_rel', (obj) => {
+    db.createRelationship(obj.src, obj.dest, obj.title, obj.description);
+  });
 };
