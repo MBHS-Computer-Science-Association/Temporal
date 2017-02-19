0915ebee78147574dfdfaf01bd2cea255a65050b
Will Robinson <sumodude529@gmail.com>
2017-02-19 00:16:07 -0600

    Update routes.js
    added view engine

--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,7 @@ var router = express.Router();
 
 router.use(express.static('public'));
 
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'ejs');
+
 module.exports = router;
